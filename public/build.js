87f4e540edb8b6012555c315dabb4c817c56bb79
zigzackattack <zackbabtkis@gmail.com>
2013-07-05 14:54:48 -0700

    added r.js buildfile

---
+++ public/build.js
@@ -0,0 +1,19 @@
+({
+	baseUrl: "./js",
+	dir: "./js/dist",
+	name: "app",
+	paths: {
+		'backbone': '../components/backbone/backbone',
+		'backbone.kendowidget': '../components/backbone.kendoWidget/backbone.kendowidget',
+		'underscore': '../components/underscore/underscore',
+		'domReady': '../components/requirejs-domready/domReady',
+		'spin': '../components/spin.js/spin',
+		'text': '../components/requirejs-text/text',
+		'jquery': '../components/jquery/jquery',
+		'modernizr': '../components/modernizr/modernizr',
+		'socketio': 'empty:',
+		'kendo': '../components/kendo-ui/js/kendo.web.min',
+		'datejs': '../components/datejs/build/date',
+		'bookmarkr': 'Admin/bookmarkr'
+	}
+})
